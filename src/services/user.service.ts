2a35a3dae5e955ef503770073376a6057664d553
bagdenius <bagdensparrow@gmail.com>
2026-02-27 18:23:04 +0200

    Implement User Service

---
+++ src/services/user.service.ts
@@ -0,0 +1,23 @@
+import { API_URL } from '@/shared/config';
+import { IUser } from '@/shared/types';
+
+import { axiosWithAuth } from '@/api';
+
+class UserService {
+	async getProfile() {
+		const response = await axiosWithAuth<IUser>({
+			url: API_URL.users('/profile'),
+			method: 'GET',
+		});
+		return response;
+	}
+
+	async toggleFavorite(productId: string) {
+		return await axiosWithAuth({
+			url: API_URL.users(`/profile/favorites/${productId}`),
+			method: 'PATCH',
+		});
+	}
+}
+
+export const userService = new UserService();
