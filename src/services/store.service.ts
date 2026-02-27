b036c30169db2401024d208ce45ca66bbca00c2b
bagdenius <bagdensparrow@gmail.com>
2026-02-27 18:13:08 +0200

    Implement Store Service

---
+++ src/services/store.service.ts
@@ -0,0 +1,41 @@
+import { API_URL } from '@/shared/config';
+import { IStore, IStoreCreate, IStoreUpdate } from '@/shared/types';
+
+import { axiosClassic, axiosWithAuth } from '@/api';
+
+class StoreService {
+	async getById(storeId: string) {
+		const { data } = await axiosClassic<IStore[]>({
+			url: API_URL.stores(`${storeId}`),
+			method: 'GET',
+		});
+		return data;
+	}
+
+	async create(data: IStoreCreate) {
+		const { data: created } = await axiosWithAuth<IStore>({
+			url: API_URL.stores(),
+			method: 'POST',
+			data,
+		});
+		return created;
+	}
+
+	async update(storeId: string, data: IStoreUpdate) {
+		const { data: updated } = await axiosWithAuth<IStore>({
+			url: API_URL.stores(`${storeId}`),
+			method: 'PUT',
+			data,
+		});
+		return updated;
+	}
+
+	async delete(storeId: string) {
+		await axiosWithAuth({
+			url: API_URL.stores(`${storeId}`),
+			method: 'DELETE',
+		});
+	}
+}
+
+export const storeService = new StoreService();
